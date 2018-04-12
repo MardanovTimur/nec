ce98f762232069ec5436c9b92a44b15f641eaf35
Ivan <iashevyakov@mail.ru>
2018-04-12 12:55:36 +0300

    adding f2-f3 arguments

--- library/lib.py
+++ library/lib.py
@@ -1,10 +1,14 @@
+#coding=utf-8
 import argparse
 
 PARSER = argparse.ArgumentParser()
 
+
 def parse_args():
-    PARSER.add_argument('-a','--argument', help='Help for this argument')
+    PARSER.add_argument('--lm', type=str, help=u'путь к сериализованной языковой модели', required=False)
+    PARSER.add_argument('--src-test-texts', type=str, help=u'путь к тестовой коллекции', required=True)
+    PARSER.add_argument('--src-texts', type=str, help=u'путь к коллекции', required=True)
+    PARSER.add_argument('-o-texts', type=str, help=u'путь для сохранения языковой модели', required=False)
 
     args = PARSER.parse_args()
     return args
-
--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from library.lib import parse_args
 class App(object):
 
     def __init__(self, args):
-        print args
-        pass
+        print(args)
+        self.lm = args.lm
+        self.src_test_texts = args.src_test_texts
+        self.src_texts = args.src_texts
+        self.o_texts = args.o_texts
+
+
+
 
 if __name__ == '__main__':
     args = parse_args()
