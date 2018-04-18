402a083073203e9092a17268ae29178960bf8fc9
Mardanov Timur Rustemovich <timurmardanov97@gmail.com>
2018-04-18 17:26:23 +0300

    Add counting and list of documents

--- library/lib.py
+++ library/lib.py
@@ -1,18 +1,55 @@
 #coding=utf-8
-import argparse
+import argparse, os, sys, fnmatch, io, re, nltk.sem, logging
+from nltk import sent_tokenize, word_tokenize, pos_tag
+
+# logger basic config
+#  logging.basicConfig(filename="../sample.log", level=logging.INFO)
 
 
+
+#   Local path of project
+PROJECT_PATH = os.path.abspath(os.path.join(__file__, '..'))
+
+#   Path of corpuses
+DATA_PATH = os.path.join(PROJECT_PATH, 'data')
+
+#   Word help in --help function
 WORD_HELP = u'<surface_all | surface_no_pm | stem | suffix_X>, где в случае surfa\
     ce_all в качестве слов берутся все токены как есть, в случае surface_no_pm – все\
         токены, кроме знаков пунктуаций, в случае stem – “стемма” (см. http://snowball.\
         tartarus.org/ ), в случае suffix_X – окончания слов длиной X'
 
-PARSER = argparse.ArgumentParser(description="Shevyakov, Mardanov")
+# Define parses for --help functions
+PARSER = argparse.ArgumentParser(description=" Project #1 \nShevyakov, Mardanov")
 
+# Choice values in parameters for app
 WORD_TYPES = ('surface_all', 'surface_no_pm', 'stem', 'suffex_X')
 FEATURES_TYPES = (False, True)
 
+# Dummy
 REQUIRED = False
+
+# Patterns for files in corpuses
+FILE_PATTERN_IN_CORPUS = {
+    u'MADE-1.0' : '[0-9]+_[0-9]+',
+    u'corpus_release' : '',
+}
+
+
+'''
+    return count(int), document_names(list of strings)
+'''
+def get_filenames_and_count_of_documents(corpus_path):
+    _mathces = []
+    re_pattern = FILE_PATTERN_IN_CORPUS.get(corpus_path.split("/")[0], None)
+    if re_pattern:
+        for root, dirnames, filenames in os.walk(os.path.abspath(os.path.join(os.path.abspath(__file__),'..','corpuses',))):
+            for filename in fnmatch.filter(filenames, re_pattern):
+                _mathces.append(os.path.join(root, filename))
+        return len(_mathces), _mathces
+    else:
+        return 0, ()
+
 
 '''
     Functional requirements
@@ -22,7 +59,7 @@ def parse_args():
         F1
     '''
     RPARSER = PARSER.add_argument_group('required arguments')
-    RPARSER.add_argument('--src-train-texts', type=str,help=u'путь к корпусу, обязательный аргумент',
+    RPARSER.add_argument('--src-train-texts', type=str,help=u'путь к корпусу, обязательный аргумент, \n Данные должны лежать в директории проекта data',
                         metavar='SRC_TRAIN_TEXTS', required=REQUIRED, nargs='?')
     PARSER.add_argument('--text-encoding', type=str,help=u'кодировка-текста в файлах корпуса',
                         metavar='Encoding', required=False, nargs='?')
@@ -46,3 +83,5 @@ def parse_args():
 
     args = PARSER.parse_args()
     return args
+
+
