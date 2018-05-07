3c4a0ff57830a280e49506b26d2b405e309bbd49
Mardanov Timur Rustemovich <timurmardanov97@gmail.com>
2018-05-07 20:43:38 +0300

    3task A realisation confirmed

--- models/pipeline.py
+++ models/pipeline.py
@@ -15,21 +15,23 @@ class PipeLine(object):
     leftVec = None
     rightVec = None
     classifyer = None
+    classifyer_n_jobs=4
+    classifyer_solver='lbfgs'
 
-
-    '''
-        test_counts - barier for classification data.
-        Example : fit data, which count less than test_counts = 200
-        if None then classificator hasn't barier
-    '''
-    def __init__(self, app, test_counts = None):
+    def __init__(self, app, ):
+        '''
+            Init pipeline
+        '''
         self.app = app
-        self.test_counts = test_counts
         self.leftVec = self.get_vectorizer(app)
         self.rightVec = self.get_vectorizer(app)
-        self.classifyer = LogisticRegression(n_jobs=4, solver='lbfgs')
+        self.classifyer = LogisticRegression(n_jobs=self.classifyer_n_jobs,
+                                             solver=self.classifyer_solver)
 
     def get_vectorizer(self, app):
+        '''
+            Get TfidfVectorizer instance
+        '''
         return TfidfVectorizer(
             encoding=app.text_encoding,
             ngram_range=(app.n, ) * 2,
@@ -39,15 +41,27 @@ class PipeLine(object):
         )
 
     def fit(self, left_words, right_words, types):
+        '''
+            Fit model
+        '''
+        self.target = types
         lData = self.leftVec.fit_transform(left_words).toarray()
         rData = self.rightVec.fit_transform(right_words).toarray()
         self.matrix = np.append(lData, rData, axis=1)
-        if self.test_counts is not None:
-            self.classifyer.fit(self.matrix[:self.test_counts], types[:self.test_counts])
-        else:
-            self.classifyer.fit(self.matrix, types)
+        self.classifyer.fit(self.matrix, self.target)
+
+    def refit(self, ):
+        '''
+            Refit model (with new features)
+        '''
+        self.classifyer = LogisticRegression(n_jobs=self.classifyer_n_jobs,
+                                             solver=self.classifyer_solver)
+        self.classifyer.fit(self.matrix, self.target)
 
     def transform(self, left_words, right_words):
+        '''
+            Get transformed left and right words from TfIdfVectorizer
+        '''
         lData = self.leftVec.transform(left_words).toarray()
         rData = self.rightVec.transform(right_words).toarray()
         self.test_data = np.append(lData, rData, axis=1)
@@ -55,9 +69,9 @@ class PipeLine(object):
     def test(self,):
         return self.classifyer.predict(self.test_data)
 
-#------------------------------------------------------------------------
-#                      3task (A part)
-#------------------------------------------------------------------------
+    #------------------------------------------------------------------------
+    #                      3task (A part)
+    #------------------------------------------------------------------------
 
     #A1
     def ref_in_one_cpos(self):
@@ -70,60 +84,64 @@ class PipeLine(object):
     def ref_in_one_wvnull(self):
         #WVNULL
         print 'WVNULL calculation'
-        self.matrix = np.hstack([self.matrix, np.array(calculate_wvnull_in_one_sentence(self.app)).T])
+        self.matrix = np.append(self.matrix, np.array([calculate_wvnull_in_one_sentence(self.app)]).T, axis=1)
 
     #A3
     def ref_in_one_wvfl(self):
         #WVFL
         print 'WVFL calculation'
-        self.matrix = np.hstack([self.matrix, np.array(calculate_wvfl_in_one_sentence(self.app)).T])
+        self.matrix = np.hstack([self.matrix, np.array([calculate_wvfl_in_one_sentence(self.app)]).T])
 
     #A4
     def ref_in_one_wbnull(self):
         #WBNULL
         print 'WBNULL calculation'
-        self.matrix = np.hstack([self.matrix, np.array(calculate_wbnull_in_one_sentence(self.app)).T])
+        self.matrix = np.hstack([self.matrix, np.array([calculate_wbnull_in_one_sentence(self.app)]).T])
 
     #A5
     def ref_in_one_wbfl(self):
         #WBFL
         print 'WBFL calculation'
-        self.matrix = np.hstack([self.matrix, np.array(calculate_wbfl_in_one_sentence(self.app)).T])
+        self.matrix = np.hstack([self.matrix, np.array([calculate_wbfl_in_one_sentence(self.app)]).T])
 
 
-#------------------------------------------------------------------------
-#                      3task ты что дурак?
-#------------------------------------------------------------------------
+    #------------------------------------------------------------------------
+    #                      3task ты что дурак?
+    #------------------------------------------------------------------------
     #C1
     def ref_in_diff_sdist(self):
         #SDIST
         print 'SDIST calculation'
-        self.matrixA = np.hstack([self.matrix,np.array(calculate_sdist_in_diff_sentence(self.app)).T])
+        self.matrixA = np.hstack([self.matrix,np.array([calculate_sdist_in_diff_sentence(self.app)]).T])
     #C2
     def left_entity_freq_in_doc(self):
         #CRFQ
         print 'CRFQ calculation'
-        self.matrixA = np.hstack([self.matrix, np.array(calculate_entity_freq_in_doc_in_diff_sentence(self.app,left=True)).T])
+        self.matrixA = np.hstack([self.matrix, np.array([calculate_entity_freq_in_doc_in_diff_sentence(self.app,left=True)]).T])
 
     #C3
     def right_entity_freq_in_doc(self):
         # DRFQ
         print 'CRFQ calculation'
-        self.matrixA = np.hstack([self.matrix, np.array(calculate_entity_freq_in_doc_in_diff_sentence(self.app, left=False)).T])
+        self.matrixA = np.hstack([self.matrix, np.array([calculate_entity_freq_in_doc_in_diff_sentence(self.app, left=False)]).T])
 
 #------------------------------------------------------------------------
 #                      3task (C part)
 #------------------------------------------------------------------------
 
+
+
+#=============================================================================
+#                       3 task A realisation
+#=============================================================================
+
 def verb_in_sentence(tagged):
-    __doc__ = 'LIB'
     if len(tagged) == 0:
         return 0
     else:
         return int(u'VERB' in tagged[:,1])
 
 def only_one_verb_in_sentence(tagged):
-    __doc__ = 'LIB'
     if len(tagged) == 0:
         return 0
     else:
@@ -133,7 +151,7 @@ def only_one_verb_in_sentence(tagged):
 def calculate_cpos_in_one_sentence(app):
     references = app.all_references
     return map(lambda reference: pos_tag(word_tokenize(reference.refAobj.value),
-                         tagset='universal')[0][1] if reference.feature_type == Features.InOneSentence else -1,references)
+                         tagset='universal')[0][1] if reference.feature_type == Features.InOneSentence else 'NO',references)
 
 # no verb between
 def calculate_wvnull_in_one_sentence(app):
@@ -156,6 +174,10 @@ def calculate_wbnull_in_one_sentence(app):
 def calculate_wbfl_in_one_sentence(app):
     references = app.all_references
     return map(lambda reference: int(len(reference.tokenized_text_between) == 1) if reference.feature_type == Features.InOneSentence else -1,references)
+
+#=============================================================================
+#                       3 task B realisation( осторожно. снизу кривой код)
+#=============================================================================
 
 def calculate_sdist_in_diff_sentence(app):
     #column of matrix with SDIST feature
